9501aa639e75aed4fb0edfb3fde553cd3cc950ba
lgferreiracic <lgferreira.cic@uesc.br>
2025-02-19 20:23:00 -0300

    Atualização do diagrama e inclusão de libs

--- EmbarcaTech_U7_Projeto.c
+++ EmbarcaTech_U7_Projeto.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/adc.h"   
+#include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "inc/ssd1306.h"  
+#include "ws2812.pio.h"
 
 int main(){
     stdio_init_all();
